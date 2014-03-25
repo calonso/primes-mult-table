7e7a3b3a845c4cfc92a409e190ea7b44bcd079ba
Carlos Alonso <calonso@mbp-de-carlos.lan>
2014-03-25 22:54:18 +0000

    created first version of the algorithm

--- lib/primes_finder.rb
+++ lib/primes_finder.rb
@@ -1,6 +1,44 @@
-
+#
+# This class uses a very simple version of Eratosthenes' sieve prime number generator
+#
 class PrimesFinder
 
+  def self.find(count)
 
+    if count.is_a?(Fixnum) && count >= 0
+      return [] if count == 0
+      # Experimentally tested that a prime number is never bigger than its position's square (at least until position 3.948.169)
+      top_limit = [count * count, 2].max 
+      primes = []
+      list = (0..top_limit).to_a
+      index = 2
+
+      while index < list.count
+        # While we have covered the whole list and
+        # We are pointing to a valid prime numebr
+        p = list[index]
+        primes << p
+
+        break if primes.count == count
+
+        # Mark every multiple of the prime number but smaller than the limit as not passing the sieve
+        mult = 2
+        while (aux = mult * p) <= top_limit
+          list[aux] = nil
+          mult += 1
+        end
+
+        # Look for the next not null number (prime)
+        while (index += 1) <= top_limit
+          break if list[index]
+        end
+      end
+      
+      primes
+    else
+      raise ArgumentError.new "#{count.class} is not accepted as a valid argument"
+    end
+
+  end
 
 end
--- spec/primes_finder_spec.rb
+++ spec/primes_finder_spec.rb
@@ -1,8 +1,53 @@
 
 require './lib/primes_finder'
+require 'prime'
 
 describe PrimesFinder do
   
-  
+  describe 'find method' do
+
+
+    describe 'parameters validation' do
+
+      it 'accepts a positive, integer number' do
+        [0, 1, 10].each do |param|
+          PrimesFinder.find(param).should be_a Array
+        end
+      end
+
+      it 'rejects invalid parameters' do
+        [-1, 'abc', 1.1].each do |param|
+          expect do
+            PrimesFinder.find param
+          end.to raise_error ArgumentError
+        end
+      end
+    end
+
+    describe 'primes finding' do
+
+      it 'returns empty array if required' do
+        PrimesFinder.find(0).should == []
+      end
+
+      it 'works for just one required' do
+        PrimesFinder.find(1).should == [2]
+      end
+
+      it 'works for 30' do
+        PrimesFinder.find(30).should == Prime.first(30)
+      end
+=begin
+      it 'works with an intermediate number' do
+        PrimesFinder.find(10000).should == Prime.first(10000)
+      end
+
+      it 'works with a huge number' do
+        PrimesFinder.find(3948169).should == Prime.first(3948169)
+      end
+=end
+    end
+
+  end
 
 end
